0a4000fbed437ad1bbab827ac70386757ea6980b
Volkan Özçelik <volkan.ozcelik@gmail.com>
2016-12-01 19:32:53 +0000

    package.json update; webpack.config simplification.

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,17 @@
-const webpack = require( 'webpack' );
 const join = require( 'path' ).join;
 const validate = require( 'webpack-validator' );
 
 module.exports = validate({
-    entry: {
-        app: join( __dirname, 'src/app.js' )
-    },
-    output: {
-        path: __dirname,
-        filename: 'scripts/[name].js',
-        pathinfo: false
-    },
+    entry: { app: join( __dirname, 'src/app.js' ) },
+    output: { path: __dirname, filename: 'scripts/[name].js', pathinfo: false },
     resolve: {
         extensions: [ '', '.webpack.js', 'web.js', '.js', '.json' ],
-        alias: {
-
-            // TODO: temporary hack
-            '__smartface_npm__': join( __dirname, '__smartface_npm__' ),
-
-            'languages': join( __dirname, 'resources/i18n' )
-        }
+        alias: { 'languages': join( __dirname, 'resources/i18n' ) }
     },
     devtool: 'source-map',
     module: {
         loaders: [
-            { test: /\.js$/, loader: 'babel', exclude: /vendor|node_modules|bower_components/ },
+            { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
             { test: /\.json$/, loader: 'json' }
         ]
     },
