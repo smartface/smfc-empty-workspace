35a4c9106771d14714fb54b6728b9e24189c370c
Ozcan Ovunc <ozcan.ovunc@smartface.io>
2017-02-22 16:57:54 +0000

    Updated page2.pgx

--- scripts/ui/page1.js
+++ scripts/ui/page1.js
@@ -9,6 +9,7 @@ var page1 = new SMF.UI.Page({
 	fillColor: "#EEEEEE"
 });
 
+
 var btn = new SMF.UI.TextButton({
 	name: "btn",
 	width: "70%",
@@ -17,8 +18,8 @@ var btn = new SMF.UI.TextButton({
 	top: "70%",
 	text: "Click me!"
 });
-
 page1.add(btn);
+
 
 var lbl = new SMF.UI.Label({
 	name: "lbl",
@@ -29,8 +30,8 @@ var lbl = new SMF.UI.Label({
 	multipleLine: true,
 	textAlignment: SMF.UI.TextAlignment.CENTER
 });
-
 page1.add(lbl);
+
 
 var img = new SMF.UI.Image({
 	name: "img",
@@ -41,8 +42,8 @@ var img = new SMF.UI.Image({
 	image: "smartface.png",
 	imageFillType: SMF.UI.ImageFillType.ASPECTFIT
 });
-
 page1.add(img);
+
 
 var btnNext = new SMF.UI.TextButton({
 	name: "btnNext",
@@ -52,32 +53,28 @@ var btnNext = new SMF.UI.TextButton({
 	top: "80.8352144469526%",
 	text: "Next Page"
 });
-
 page1.add(btnNext);
 
 
-page1.onShow = function() {
-	SMF.UI.statusBar.name = "statusBar";
-	SMF.UI.statusBar.style = SMF.UI.StatusBarStyle.DEFAULT;
-	if(Device.deviceOS === 'iOS'){
-		SMF.UI.iOS.NavigationBar.name = "navigationBar";
-	}
-	if(Device.deviceOS === 'Android'){
-		page1.actionBar.name = "actionBar";
-	}
 
-	if(Device.deviceOS === 'iOS')
-		SMF.UI.iOS.NavigationBar.visible = false;
-	if(Device.deviceOS === 'Android')
-		page1.actionBar.visible = false;
-	SMF.UI.statusBar.visible = false;
+page1.onShow = function() {
+    SMF.UI.statusBar.name = "statusBar";
+    SMF.UI.statusBar.visible = false;
+    if(Device.deviceOS === 'iOS'){
+        SMF.UI.iOS.NavigationBar.name = "navigationBar";
+        SMF.UI.iOS.NavigationBar.visible = false;
+    }
+    if(Device.deviceOS === 'Android'){
+        page1.actionBar.name = "actionBar";
+        page1.actionBar.visible = false;
+    }
 };
 
 Object.assign(page1, {
-	btn: btn,
-	lbl: lbl,
-	img: img,
-	btnNext: btnNext,
+    btn: btn,
+    lbl: lbl,
+    img: img,
+    btnNext: btnNext
 });
 
 module && (module.exports = page1);
--- scripts/ui/page2.js
+++ scripts/ui/page2.js
@@ -4,10 +4,11 @@
 		Do not modify it's contents.
 */
 
-var page1 = new SMF.UI.Page({
-	name: "page1",
+var page2 = new SMF.UI.Page({
+	name: "page2",
 	fillColor: "#B7D21C"
 });
+
 
 var btn = new SMF.UI.TextButton({
 	name: "btn",
@@ -17,8 +18,8 @@ var btn = new SMF.UI.TextButton({
 	top: "80.8352144469526%",
 	text: "Back"
 });
+page2.add(btn);
 
-page1.add(btn);
 
 var lbl = new SMF.UI.Label({
 	name: "lbl",
@@ -30,8 +31,8 @@ var lbl = new SMF.UI.Label({
 	text: "This is second page",
 	textAlignment: SMF.UI.TextAlignment.CENTER
 });
+page2.add(lbl);
 
-page1.add(lbl);
 
 var img = new SMF.UI.Image({
 	name: "img",
@@ -42,31 +43,27 @@ var img = new SMF.UI.Image({
 	image: "smartface.png",
 	imageFillType: SMF.UI.ImageFillType.ASPECTFIT
 });
+page2.add(img);
 
-page1.add(img);
 
 
-page1.onShow = function() {
-	SMF.UI.statusBar.name = "statusBar";
-	SMF.UI.statusBar.style = SMF.UI.StatusBarStyle.DEFAULT;
-	if(Device.deviceOS === 'iOS'){
-		SMF.UI.iOS.NavigationBar.name = "navigationBar";
-	}
-	if(Device.deviceOS === 'Android'){
-		page1.actionBar.name = "actionBar";
-	}
-
-	if(Device.deviceOS === 'iOS')
-		SMF.UI.iOS.NavigationBar.visible = false;
-	if(Device.deviceOS === 'Android')
-		page1.actionBar.visible = false;
-	SMF.UI.statusBar.visible = false;
+page2.onShow = function() {
+    SMF.UI.statusBar.name = "statusBar";
+    SMF.UI.statusBar.visible = false;
+    if(Device.deviceOS === 'iOS'){
+        SMF.UI.iOS.NavigationBar.name = "navigationBar";
+        SMF.UI.iOS.NavigationBar.visible = false;
+    }
+    if(Device.deviceOS === 'Android'){
+        page2.actionBar.name = "actionBar";
+        page2.actionBar.visible = false;
+    }
 };
 
-Object.assign(page1, {
-	btn: btn,
-	lbl: lbl,
-	img: img,
+Object.assign(page2, {
+    btn: btn,
+    lbl: lbl,
+    img: img
 });
 
-module && (module.exports = page1);
+module && (module.exports = page2);
