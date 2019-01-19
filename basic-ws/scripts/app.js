3259c01aef98be69ff0dee0dc846b4166699fc13
Ali Tuğrul PINAR <ali.pinar@smartface.io>
2019-01-19 18:38:58 +0000

    Add gitignore

--- basic-ws/scripts/app.js
+++ basic-ws/scripts/app.js
@@ -24,38 +24,6 @@ notifier.subscribe((connectionType) => {
     }
 });
 
-const {
-    NativeRouter: Router,
-    NativeStackRouter: StackRouter,
-    Route
-} = require("@smartface/router");
-
-const router = Router.of({
-    path: "/",
-    isRoot: true,
-    routes: [
-        StackRouter.of({
-            path: "/pages",
-            routes: [
-                Route.of({
-                    path: "/pages/page1",
-                    build: (router, route) => {
-                        const { routeData, view } = route.getState();
-                        let Page1 = require("pages/page1");
-                        return new Page1(routeData, router);
-                    }
-                }),
-                Route.of({
-                    path: "/pages/page2",
-                    build: (router, route) => {
-                        const { routeData, view } = route.getState();
-                        let Page2 = require("pages/page2");
-                        return new Page2(routeData, router);
-                    }
-                })
-            ]
-        })
-    ]
-});
+const router = require("./routes");
 
 router.push("/pages/page1");
--- sliderdrawer-ws/scripts/app.js
+++ sliderdrawer-ws/scripts/app.js
@@ -24,51 +24,10 @@ notifier.subscribe((connectionType) => {
     }
 });
 
-const {
-    NativeRouter: Router,
-    NativeStackRouter: StackRouter,
-    Route
-} = require("@smartface/router");
-const sliderDrawerWrapper = require("./sliderdrawer-comp");
-
-const router = Router.of({
-    path: "/",
-    isRoot: true,
-    routes: [
-        StackRouter.of({
-            path: "/pages",
-            routes: [
-                Route.of({
-                    path: "/pages/page1",
-                    routeDidEnter: (router, route) => {
-                        sliderDrawerWrapper.enabled = true;
-                        sliderDrawerWrapper.hide();
-                    },
-                    routeDidExit: (router, route) => {},
-                    build: (router, route) => {
-                        const { routeData, view } = route.getState();
-                        let Page1 = require("pages/page1");
-                        return new Page1(routeData, router, sliderDrawerWrapper);
-                    }
-                }),
-                Route.of({
-                    path: "/pages/page2",
-                    routeDidEnter: (router, route) => {
-                        sliderDrawerWrapper.enabled = true;
-                        sliderDrawerWrapper.hide();
-                    },
-                    routeDidExit: (router, route) => {},
-                    build: (router, route) => {
-                        const { routeData, view } = route.getState();
-                        let Page2 = require("pages/page2");
-                        return new Page2(routeData, router, sliderDrawerWrapper);
-                    }
-                })
-            ]
-        })
-    ]
-});
 //set router to sliderdrawer for action of options.
-sliderDrawerWrapper.setRouter(router);
 //go page1
-router.push("/pages/page1");
+
+const router = require("./routes");
+
+router.push("/btb/tab1/page1");
+
